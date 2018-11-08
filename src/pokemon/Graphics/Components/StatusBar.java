52c0ac168e5d0fca703b6db78bbb390b3b020534
Amjad Alsharafi <amjadsharafi10@gmail.com>
2018-11-08 16:01:20 +0800

    Add: StatusBar custom javafx component.
    - packages the status of a pokemon.

---
+++ src/pokemon/Graphics/Components/StatusBar.java
@@ -0,0 +1,54 @@
+package pokemon.Graphics.Components;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.HBox;
+
+import java.io.IOException;
+
+public class StatusBar extends HBox {
+
+	@FXML
+	private Label name;
+
+	@FXML
+	private ProgressBar health;
+
+	@FXML
+	private ProgressBar accumulator;
+
+	public StatusBar() throws IOException {
+
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("status_bar.fxml"));
+		loader.setRoot(this);
+		loader.setController(this);
+
+		loader.load();
+	}
+
+	public void setHealth(double value) {
+		health.progressProperty().set(value);
+	}
+
+	public double getHealth() {
+		return health.progressProperty().get();
+	}
+
+	public double getAccumulator() {
+		return accumulator.progressProperty().get();
+	}
+
+	public void setAccumulator(double value) {
+		accumulator.progressProperty().set(value);
+	}
+
+	public String getName() {
+		return name.textProperty().get();
+	}
+
+	public void setName(String value) {
+		name.textProperty().set(value);
+	}
+}
--- src/pokemon/Graphics/Controllers/Main.java
+++ src/pokemon/Graphics/Controllers/Main.java
@@ -2,10 +2,34 @@ package pokemon.Graphics.Controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
-
+import javafx.scene.layout.VBox;
 
 public class Main {
 
+	private final double
+			ROOT_HEIGHT = 800,
+			ROOT_WIDTH = 1200,
+			STATUS_HEIGHT = (50 + 10) * 3,
+			STATUS_WIDTH = 250,
+			MARGIN_SIZE = 20;
+
+
 	@FXML
 	private Pane root;
+	@FXML
+	private VBox status_opponent;
+	@FXML
+	private VBox status_player;
+
+	@FXML
+	private void initialize(){
+		double status_allY = ROOT_HEIGHT - MARGIN_SIZE - STATUS_HEIGHT,
+				status_opponentX = MARGIN_SIZE,
+				status_playerX = ROOT_WIDTH - MARGIN_SIZE - STATUS_WIDTH;
+
+		status_opponent.setLayoutX(status_opponentX);
+		status_opponent.setLayoutY(status_allY);
+		status_player.setLayoutX(status_playerX);
+		status_player.setLayoutY(status_allY);
+	}
 }
