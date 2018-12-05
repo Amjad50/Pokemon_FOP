7c2c5ab3f02d6d19f15ab630bc7926154b816d01
Amjad Alsharafi <amjadsharafi10@gmail.com>
2018-12-05 15:49:30 +0800

    Edit: change to dynamic sizing of objects, Add: StatusGroup that holds StatuBars.
    - made most items(not all) depend on root size.

---
+++ src/FinalMonster/Graphics/Constrains.java
@@ -0,0 +1,6 @@
+package FinalMonster.Graphics;
+
+public class Constrains {
+	public static double ROOT_HEIGHT = 800,
+			ROOT_WIDTH = 1200;
+}
--- src/FinalMonster/Graphics/Controllers/Main.java
+++ src/FinalMonster/Graphics/Controllers/Main.java
@@ -1,5 +1,6 @@
 package FinalMonster.Graphics.Controllers;
 
+import FinalMonster.Graphics.Constrains;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.layout.Pane;
@@ -8,11 +9,25 @@ import javafx.scene.layout.VBox;
 public class Main {
 
 	private final double
-			ROOT_HEIGHT = 800,
-			ROOT_WIDTH = 1200,
-			STATUS_HEIGHT = ( 50 + 10 ) * 3,
-			STATUS_WIDTH = 250,
+			ROOT_STATUS_GROUP_RATIO_H = 40.0 / 9.0,
+			ROOT_STATUS_GROUP_RATIO_W = 24.0/ 5.0,
+			STATUS_SPACING = 10,
+			N_STATUS_ELEMENTS = 3,
 			MARGIN_SIZE = 20;
+
+	private double STATUS_GROUP_HEIGHT,
+			STATUS_GROUP_WIDTH,
+			STATUS_WIDTH,
+			STATUS_HEIGHT;
+
+	{
+		double root_h = Constrains.ROOT_HEIGHT, root_w = Constrains.ROOT_WIDTH;
+
+		STATUS_GROUP_HEIGHT = root_h  / ROOT_STATUS_GROUP_RATIO_H;
+		STATUS_GROUP_WIDTH = root_w / ROOT_STATUS_GROUP_RATIO_W;
+		STATUS_HEIGHT = (STATUS_GROUP_HEIGHT - (N_STATUS_ELEMENTS - 1) * 10) / N_STATUS_ELEMENTS;
+		STATUS_WIDTH = STATUS_GROUP_WIDTH;
+	}
 
 
 	@FXML
@@ -24,14 +39,29 @@ public class Main {
 
 	@FXML
 	private void initialize() {
-		double status_allY = ROOT_HEIGHT - MARGIN_SIZE - STATUS_HEIGHT,
+		initSizes();
+
+		double status_allY = Constrains.ROOT_HEIGHT - MARGIN_SIZE - STATUS_GROUP_HEIGHT,
 				status_opponentX = MARGIN_SIZE,
-				status_playerX = ROOT_WIDTH - MARGIN_SIZE - STATUS_WIDTH;
+				status_playerX = Constrains.ROOT_WIDTH - MARGIN_SIZE - STATUS_GROUP_WIDTH;
 
 		status_opponent.setLayoutX(status_opponentX);
 		status_opponent.setLayoutY(status_allY);
 		status_player.setLayoutX(status_playerX);
 		status_player.setLayoutY(status_allY);
+	}
+
+	private void initSizes() {
+
+		root.setPrefHeight(Constrains.ROOT_HEIGHT);
+		root.setPrefWidth(Constrains.ROOT_WIDTH);
+
+		status_opponent.setPrefHeight(STATUS_GROUP_HEIGHT);
+		status_opponent.setPrefWidth(STATUS_GROUP_WIDTH);
+		status_opponent.setSpacing(STATUS_SPACING);
+		status_player.setPrefHeight(STATUS_GROUP_HEIGHT);
+		status_player.setPrefWidth(STATUS_GROUP_WIDTH);
+		status_player.setSpacing(STATUS_SPACING);
 	}
 
 	public void addChild(Node node) {
