a37e8bb5b6bedc67955c471608ef7b2abf012439
Amjad Alsharafi <amjadsharafi10@gmail.com>
2018-12-11 21:10:54 +0800

    Edit: used one type for each pokemon and added id
    - also some code cleaning
    - id is of type String

--- src/FinalMonster/Parser/Move.java
+++ src/FinalMonster/Parser/Move.java
@@ -1,65 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package FinalMonster.Parser;
 
-/**
- *
- * @author User
- */
 class Move {
-    private String name;
-    private String type;
-    private int power;
-    private int accuracy;
+	private String name;
+	private String type;
+	private int power;
+	private int accuracy;
 
-    public Move(String name, String type, int power,int accuracy) {
-        this.name = name;
-        this.type = type;
-        this.power = power;
-        this.accuracy = accuracy;
-    }
+	public Move(String name, String type, int power, int accuracy) {
+		this.name = name;
+		this.type = type;
+		this.power = power;
+		this.accuracy = accuracy;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setType(String type) {
-        this.type = type;
-    }
+	public void setType(String type) {
+		this.type = type;
+	}
 
-    public void setPower(int power) {
-        this.power = power;
-    }
+	public void setPower(int power) {
+		this.power = power;
+	}
 
-    public void setAccuracy(int accuracy) {
-        this.accuracy = accuracy;
-    }
+	public void setAccuracy(int accuracy) {
+		this.accuracy = accuracy;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getType() {
-        return type;
-    }
+	public String getType() {
+		return type;
+	}
 
-    public int getPower() {
-        return power;
-    }
+	public int getPower() {
+		return power;
+	}
 
-    public int getAccuracy() {
-        return accuracy;
-    }
+	public int getAccuracy() {
+		return accuracy;
+	}
 
-    @Override
-    public String toString() {
-        return "\nMove: " +  name + "\nType: " + type + "\nPower: " + power + "\nAccuracy: " + accuracy;
-    }
-    
-    
-    
-    
+	@Override
+	public String toString() {
+		return "\nMove: " + name + "\nType: " + type + "\nPower: " + power + "\nAccuracy: " + accuracy;
+	}
+
+
 }
--- src/FinalMonster/Parser/Pokemon.java
+++ src/FinalMonster/Parser/Pokemon.java
@@ -1,103 +1,97 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package FinalMonster.Parser;
 
-/**
- *
- * @author User
- */
 public class Pokemon {
-    private String name;
-    private String type;
-    private int hp;
-    private int attack;
-    private int defense;
-    private int speed;
-    private Move moves[];
+	private String id;
+	private String name;
+	private String type;
+	private int hp;
+	private int attack;
+	private int defense;
+	private int speed;
+	private Move moves[];
 
-    public Pokemon(String name, String type, int hp, int attack, int defense, int speed, Move[] moves) {
-        this.name = name;
-        this.type = type;
-        this.hp = hp;
-        this.attack = attack;
-        this.defense = defense;
-        this.speed = speed;
-        this.moves = moves;
-    }
-    
-    
+	public Pokemon(String id, String name, String type, int hp, int attack, int defense, int speed, Move[] moves) {
+		this.id = id;
+		this.name = name;
+		this.type = type;
+		this.hp = hp;
+		this.attack = attack;
+		this.defense = defense;
+		this.speed = speed;
+		this.moves = moves;
+	}
 
-    public String getName() {
-        return name;
-    }
+	private String getId() {
+		return id;
+	}
 
-    public String getType() {
-        return type;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public int getHp() {
-        return hp;
-    }
+	public String getType() {
+		return type;
+	}
 
-    public int getAttack() {
-        return attack;
-    }
+	public int getHp() {
+		return hp;
+	}
 
-    public int getDefense() {
-        return defense;
-    }
+	public int getAttack() {
+		return attack;
+	}
 
-    public int getSpeed() {
-        return speed;
-    }
+	public int getDefense() {
+		return defense;
+	}
 
-    public Move[] getMoves() {
-        return moves;
-    }
+	public int getSpeed() {
+		return speed;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public Move[] getMoves() {
+		return moves;
+	}
 
-    public void setType(String type) {
-        this.type = type;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setHp(int hp) {
-        this.hp = hp;
-    }
+	public void setType(String type) {
+		this.type = type;
+	}
 
-    public void setAttack(int attack) {
-        this.attack = attack;
-    }
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
 
-    public void setDefense(int defense) {
-        this.defense = defense;
-    }
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
 
-    public void setSpeed(int speed) {
-        this.speed = speed;
-    }
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
 
-    public void setMoves(Move[] moves) {
-        this.moves = moves;
-    }
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
 
-    @Override
-    public String toString() {
-        String a =  "Pokemon: "+ name + "\nType: " + type + "\nHP: " + hp + "\nAttack: " + attack + "\nDefense: " + defense + "\nSpeed: " + speed ;
-        for(int i = 0; i < 4; i++){
-            a += moves[i].toString();
-        }
-        ;
-        
-        return a;
-    }
-    
-    
-    
-    
+	public void setMoves(Move[] moves) {
+		this.moves = moves;
+	}
+
+	@Override
+	public String toString() {
+		String a = "Pokemon(" + id + "): " + name + "\nType: " + type + "\nHP: " + hp + "\nAttack: " + attack + "\nDefense: " + defense + "\nSpeed: " + speed;
+		for ( int i = 0; i < 4; i++ ) {
+			a += moves[i].toString();
+		}
+		;
+
+		return a;
+	}
+
+
 }
