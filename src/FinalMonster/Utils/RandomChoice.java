30e7a6dd19e3f74b2884b2925bda6552349c3d3e
Amjad Alsharafi <amjadsharafi10@gmail.com>
2018-12-16 12:45:43 +0800

    Edit: edited the random method in RandomChoice and made more efficient
    - when choosing randomly it choose the least amount
    example: if src.size() == 10 and length == 9,
    it will not random 9 elements, instead will random just one element
    and exclude it from the result

--- src/FinalMonster/Utils/RandomChoice.java
+++ src/FinalMonster/Utils/RandomChoice.java
@@ -1,6 +1,7 @@
 package FinalMonster.Utils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -9,10 +10,19 @@ public class RandomChoice {
 	public static <T> ArrayList<T> random(List<T> src, int length) {
 		assert length <= src.size();
 		Random r = new Random();
-		int[] indecis = new int[length];
+		boolean reverse;
+		int lengthS;
+		if ( src.size() / 2 + 1 < length ) {
+			reverse = true;
+			lengthS = src.size() - length;
+		} else {
+			reverse = false;
+			lengthS = length;
+		}
+		int[] indecis = new int[lengthS];
 		ArrayList<T> result = new ArrayList<>(length);
 
-		for ( int i = 0; i < length; i++ ) {
+		for ( int i = 0; i < lengthS; i++ ) {
 			indecis[i] = r.nextInt(src.size());
 			for ( int j = 0; j < i; j++ ) {
 				if ( indecis[j] == indecis[i] ) {
@@ -21,8 +31,16 @@ public class RandomChoice {
 				}
 			}
 		}
-		for ( int i = 0; i < length; i++ ) {
-			result.add(src.get(indecis[i]));
+		if ( reverse ) {
+			List indciesl = Arrays.asList(indecis);
+			for ( int i = 0; i < src.size(); i++ ) {
+				if ( !indciesl.contains(i) )
+					result.add(src.get(i));
+			}
+		} else {
+			for ( int i = 0; i < length; i++ ) {
+				result.add(src.get(indecis[i]));
+			}
 		}
 		return result;
 	}
