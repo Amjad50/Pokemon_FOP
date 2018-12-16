20b17aab5b96c051b891a276d15ffb05612f56ee
Amjad Alsharafi <amjadsharafi10@gmail.com>
2018-12-17 00:42:40 +0800

    Edit: Final commit for version 0.1-beta
    - Moved the execution of main from TestMan to Main

--- src/FinalMonster/Main.java
+++ src/FinalMonster/Main.java
@@ -1,8 +1,12 @@
 package FinalMonster;
 
+import FinalMonster.Graphics.PokemonApplication;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+	public static void main(String[] args) {
+		PokemonApplication app = new PokemonApplication();
+
+		app.run();
+	}
 }
