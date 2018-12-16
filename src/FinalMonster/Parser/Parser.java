465613e409fcd613d6faa731ba808bda41a0e393
Amjad Alsharafi <amjadsharafi10@gmail.com>
2018-12-17 01:13:29 +0800

    Edit: Final Final Commit: Switch from fileStream to InputStream as resource Stream

--- src/FinalMonster/Parser/Parser.java
+++ src/FinalMonster/Parser/Parser.java
@@ -1,54 +1,46 @@
 
 package FinalMonster.Parser;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Parser {
 
 	private static final int POKEMON_ARR_SIZE = 111;
 
 	public static Pokemon[] scan() {
-		try {
-			Scanner s = new Scanner(new FileInputStream(Parser.class.getResource("pokemon.txt").getPath()));
-			Pokemon[] pokemon = new Pokemon[POKEMON_ARR_SIZE];
-			for ( int j = 0; j < POKEMON_ARR_SIZE; j++ ) {
-				String ID = s.nextLine();
-				String name = s.nextLine();
-				String type = s.nextLine();
-				int hp = s.nextInt();
-				int attack = s.nextInt();
-				int defense = s.nextInt();
-				int speed = s.nextInt();
+		Scanner s = new Scanner(new InputStreamReader(Parser.class.getResourceAsStream("pokemon.txt")));
+		Pokemon[] pokemon = new Pokemon[POKEMON_ARR_SIZE];
+		for ( int j = 0; j < POKEMON_ARR_SIZE; j++ ) {
+			String ID = s.nextLine();
+			String name = s.nextLine();
+			String type = s.nextLine();
+			int hp = s.nextInt();
+			int attack = s.nextInt();
+			int defense = s.nextInt();
+			int speed = s.nextInt();
+			s.nextLine();
+
+			Move[] moves = new Move[4];
+
+			for ( int i = 0; i < 4; i++ ) {
+				String movename = s.nextLine();
+				String movetype = s.nextLine();
+				int power = s.nextInt();
+				int accuracy = s.nextInt();
+				if ( s.hasNext() ) {
+					s.nextLine();
+				}
+				moves[i] = new Move(movename, movetype, power, accuracy);
+
+			}
+			if ( s.hasNext() )
 				s.nextLine();
 
-				Move[] moves = new Move[4];
-
-				for ( int i = 0; i < 4; i++ ) {
-					String movename = s.nextLine();
-					String movetype = s.nextLine();
-					int power = s.nextInt();
-					int accuracy = s.nextInt();
-					if ( s.hasNext() ) {
-						s.nextLine();
-					}
-					moves[i] = new Move(movename, movetype, power, accuracy);
-
-				}
-				if ( s.hasNext() )
-					s.nextLine();
-
-				pokemon[j] = new Pokemon(ID, name, type, hp, attack, defense, speed, moves);
-			}
-
-			return pokemon;
-		} catch (FileNotFoundException ex) {
-			Logger.getLogger(Parser.class.getName()).log(Level.SEVERE, null, ex);
-			return new Pokemon[0];
+			pokemon[j] = new Pokemon(ID, name, type, hp, attack, defense, speed, moves);
 		}
+
+		return pokemon;
 
 	}
 }
